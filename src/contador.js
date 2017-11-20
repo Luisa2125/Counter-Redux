5222481bc611f60db364edfd8cec6d613983b084
Luisa2125 <luiyi2125@gmail.com>
2017-11-20 00:54:47 -0600

    FINALIZADO

--- src/contador.js
+++ src/contador.js
@@ -1,22 +1,41 @@
-import {fromJS} from 'immutable'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-const SUMAR = 'SUMAR'
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
 
-export const SUMARCount = ()=>(
-	{
-		type: SUMAR
-	}
-)
-const EstadoInicial = fromJS(
-	{
-		counter: 0
-	}
-)
-
-const RealizarSuma = (state=EstadoInicial, action={})=>{
-	switch(action.type){
-		case SUMAR:
-		console.log(state+1)
-		return state + 1
-	}
+  render() {
+    const { value, onIncrement, onDecrement } = this.props
+    return (
+      <div>	
+      	<h1>Contador Redux</h1>
+	      <div className="input-group">
+	      	<span className="input-group-addon" style={{background:'#000000', color:'white'}} >Counter</span>
+	      	<input id="msg" type="text" className="form-control" name="msg" placeholder="Additional Info" style={{width:"80px"}}  value={value}/>
+	      </div>
+	      <div></div>
+	      <div className="btn-group" style={{ display:"grid",gridTemplateColumns: "100px 100px",gridTemplateRows: "30px  " }}>
+	        <button  type="button" className="btn" style={{background:'#000000', color:'white'}} onClick={onIncrement}>
+	          SUMAR
+	        </button>
+	        {' '}
+	        <button type="button" className="btn" style={{background:'#000000', color:'white'}} onClick={onDecrement}>
+	          RESTAR
+	        </button>
+	        {' '}
+	      </div>
+      </div>
+    )
+  }
 }
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
--- src/index.js
+++ src/index.js
@@ -1,60 +1,25 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import{ createStore } from 'redux';
-
-const states= []
-
-function sumaArray(array){
-  let state = 0
-  for(let i = 0;i<array.length;i++){
-    state = state + parseInt(array[i])
-  }
-  return state
-}
-function RealizarSuma(state=[], action){
-  switch(action.type){
-    
-    case 'SUMAR':
-      states.push([action.data]+1)
-      return state =sumaArray(states) 
-    case 'RESTA':
-      states.push([action.data]-1)
-      return state = sumaArray(states)
-    default:
-      return state
-  }
-}
-
-let store = createStore(RealizarSuma);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './contador.js'
+import counter from './reducer/counter.js'
 
 
-class App extends Component{
-  constructor(){
-    super()
-    store.subscribe(() =>{
-      console.log(store.getState())
-    });
-  }
-suma(){
-  console.log('sumita')
-  store.dispatch({type:'SUMAR',data:0})
-}
-resta(){
-  console.log('restita')
-  store.dispatch({type:'RESTA',data:0})
-}
+const store = createStore(counter)
+const htmlroot = document.getElementById('root')
 
-  render(){
-    return(
-      <div className = "App">
-        <h1>Simple redux counter</h1>
-        <button onClick={this.suma}>SUMAR</button>
-        <button onClick={this.resta}>RESTAR</button>
-        <div></div>
-      </div>
-    );
-  }
-}
+const render = () => ReactDOM.render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  htmlroot
+)
 
-ReactDOM.render(<App />,document.getElementById('root'))
+render()
+store.subscribe(render)
+
+
+
 
